032dda2864ba8d1895e9cceddbbccb715ab18c49
PARAMVEER SINGH DULLAT <paramdullat123@gmail.com>
2026-09-06 21:43:57 +0530

    Update contact phone numbers in footer section

--- app/components/footer-section.tsx
+++ app/components/footer-section.tsx
@@ -69,7 +69,8 @@ const FooterSection: React.FC = () => {
               </div>
               <div className="flex items-center gap-2">
                 <Phone className="h-4 w-4 text-primary" />
-                <p>+91 98765 43210</p>
+                <p>+91 86288 02887</p><br/>
+                <p>+91 82849 13118</p>
               </div>
               <div className="flex items-center gap-2">
                 <Send className="h-4 w-4 text-primary" />
